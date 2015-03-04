4ee794e64122a0aa82322574fd3bb1393138163b
Luong Hoang <lhoang@live.com>
2015-03-03 20:10:21 -0500

    upgraded to .net 4.5.2 to use QueueBackgroundItem

--- Presentation/Nop.Web/Controllers/HomeController.cs
+++ Presentation/Nop.Web/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using Nop.Web.Extensions;
 using Nop.Web.Framework.Security;
+using System.Web.Hosting;
+using System.Web.Mvc;
 
 namespace Nop.Web.Controllers
 {
@@ -8,6 +10,18 @@ namespace Nop.Web.Controllers
         [NopHttpsRequirement(SslRequirement.No)]
         public ActionResult Index()
         {
+            HostingEnvironment.QueueBackgroundWorkItem(token =>
+            {
+                DecisionServiceWrapper<string>.Create(
+                    appId: "louiemart",
+                    appToken: "c7b77291-f267-43da-8cc3-7df7ec2aeb06",
+                    //appId: "rcvtest",
+                    //appToken: "c01ff675-5710-4814-a961-d03d2d6bce65",
+                    epsilon: .2f, 
+                    numActions: 10, 
+                    modelOutputDir: HostingEnvironment.MapPath("~/")
+                );
+            });
             return View();
         }
     }
---
+++ Presentation/Nop.Web/Extensions/DecisionServiceWrapper.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MultiWorldTesting;
+using ClientDecisionService;
+
+namespace Nop.Web.Extensions
+{
+    public static class DecisionServiceWrapper<TContext>
+    {
+        public static EpsilonGreedyExplorer<TContext> Explorer { get; set; }
+        public static DecisionServiceConfiguration<TContext> Configuration { get; set; }
+        public static DecisionService<TContext> Service { get; set; }
+
+        public static void Create(string appId, string appToken, float epsilon, uint numActions, string modelOutputDir)
+        {
+            if (Explorer == null)
+            {
+                Explorer = new EpsilonGreedyExplorer<TContext>(new MartPolicy<TContext>(), epsilon, numActions);
+            }
+
+            if (Configuration == null)
+            {
+                Configuration = new DecisionServiceConfiguration<TContext>(appId, appToken, Explorer)
+                {
+                    PolicyModelOutputDir = modelOutputDir,
+                    BatchConfig = new BatchingConfiguration 
+                    {
+                        MaxDuration = TimeSpan.FromSeconds(5),
+                        MaxBufferSizeInBytes = 10,
+                        MaxEventCount = 1,
+                        MaxUploadQueueCapacity = 1,
+                        UploadRetryPolicy = BatchUploadRetryPolicy.Retry
+                    }
+                };
+            }
+
+            if (Service == null)
+            {
+                Service = new DecisionService<TContext>(Configuration);
+            }
+        }
+    }
+
+    class MartPolicy<TContext> : IPolicy<TContext>
+    {
+        public uint ChooseAction(TContext context)
+        {
+            return 5;
+        }
+    }
+}
